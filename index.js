575c3788bf736cad1830b169e1b25b1a692ad83e
Jasdeep Singh <jsing287@uwo.ca>
2021-11-30 15:07:05 -0500

    finished

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 const express = require("express")
 const app = express();
 const port = 3000;
-const { Server } = require("socket.io");
+const { Server } = require("socket.io"); //acquiring the  socket.io library to create and use socket connections.
 
-const commandInput = require("readline");
+const commandInput = require("readline"); // allows reading from the terminal.
 
-app.use(express.static("static"))
+app.use(express.static("static")) //  telling express  to use the static folder index.html as default load.
 
+
+// creating a server that listens on port 3000.
 let socketServer = app.listen(port, ()=>
 {
     console.log("Server Listening on  Port: " + port)
 })
 
-let chatUsers = {};
-let rooms = ["Community"]
+let chatUsers = {}; // a JSON to  hold  all current chat  users.
+let rooms = ["Community"]  // an array to hold all rooms that have been created within the chatroom
 
-const io = new Server(socketServer);
+const io = new Server(socketServer);  // creating an instance of a socket server.
 
+// creating an instance of a command line interface.
 const serverLine  = commandInput.createInterface({input: process.stdin, output: process.stdout})
 
 
-
+// the initial .on connect method starts when the socket  connection has been successfully made.
 io.on("connection", (socket) => {
 
+    // broadcasts a message to all clients that the socket server has been terminated.
     function killClient()
     {
-        socket.broadcast.emit("notifyChat", "ADMIN", "The Server has been Terminated!")
+        socket.broadcast.emit("notifyChat", "ADMIN", "The Server has been Terminated!") // emitter to notify the chat window  of all clients.
     }
 
 
+    // Outputs a question into the commandline asking users to enter the word "kill"  if they would like to terminate the server
     serverLine.question("Input kill to close the server ", (input)=>{
         if(input=="kill")
         {
@@ -38,61 +43,80 @@ io.on("connection", (socket) => {
     })
 
 
-    socket._error((err)=>{
-        alert(err);
-    })
+    // error handling if there is a connection error to the server.
+    socket.on("connect_error", (err) => {
+        console.log(`connect_error due to ${err.message}`);
+      });
 
 
-    
+
+      // listener event for the addUser function of the program.
     socket.on("addUser", function (name){
    
+        // sets a socket attribute userName to the name passed for easy access in other listeners and adds it to the chatUser object.
         socket.userName = name;
         chatUsers[name] = name;
+
+        // updates all client clients user lists on the front end html to the new json object list.
         io.sockets.emit("updateList", chatUsers);
-        socket.homeRoom = "Community";
+        socket.homeRoom = "Community"; // putss the user in the default community room
 
-        socket.join("Community");
+        socket.join("Community"); // joins the room
 
-        socket.emit("notifyChat", "ADMIN", " Has Entered the Chat Room." )
+        socket.emit("notifyChat", "ADMIN", name + " Has Entered the Chat Room." ) // notifies the chat that user has entered
 
-        socket.broadcast.to("Community").emit("notifyChat", name + " Has joined the chat.")
+        // broadcats to all users within the chat the user has joined the chat.
+        socket.broadcast.to("Community").emit("notifyChat", name + " Has joined the chat", "")
+        io.sockets.emit("switchRoom",  rooms) 
 
         
     });
 
+    // listener event for when a user wants to switch to a different chat room.
     socket.on("switchRoom", (newRoom)=>{
+ 
+        // notifies all users within the chat that a specific user has left. 
         socket.broadcast.to(socket.homeRoom).emit("notifyChat", "ADMIN",
         socket.userName + " Has Left This Chat Room. ");
-        socket.leave(socket.homeRoom)
-        socket.homeRoom = newRoom
-        socket.join(newRoom)
+        socket.leave(socket.homeRoom) // leaves the socket  room
+        socket.homeRoom = newRoom // sets the homeRoom attribute to the newRoom 
+        socket.join(newRoom) // joins the new Room.
+       
+        //  notifies all users within the new chat  room that the user has entered.
         socket.emit("notifyChat", "ADMIN", socket.userName + " Has Joined " +  newRoom)
         socket.broadcast.to(newRoom).emit("notifyChat", "ADMIN",
         socket.userName + " Has Entered the Chat Room.")
     });
 
+    // listener event for when an individual client socket has disconnected from the server.
     socket.on("disconnect", ()=>{
+
+        // graceful termination of the  client socket with its removal from the users json
         delete chatUsers[socket.userName];
-        io.sockets.emit("updateList", chatUsers);
-        socket.broadcast.emit("notifyChat", "ADMIN", socket.userName + " Has Left The Chat!");
-        socket.leave(socket.homeRoom)
+        io.sockets.emit("updateList", chatUsers); //  updates the user list
+        socket.broadcast.emit("notifyChat", "ADMIN", socket.userName + " Has Left The Chat!"); //  notifies users that the client has left the chat.
+        socket.leave(socket.homeRoom) // removes them from the socket room.
     });
 
 
+    // listener event  from when a new chat room is created
     socket.on("newChat", (newRoom)=>{
 
+        
+        // if the room name is not null push it into the array and run all client sockets switchRoom listener event
         if(rooms!=null)
         {
             rooms.push(newRoom);
-            io.sockets.emit("switchRoom", newRoom)
+            io.sockets.emit("switchRoom", rooms)
         }
         else
         {
-            alert("Something went wrong!")
+            alert("Something went wrong!  Room name null") // alert is the room name  is null
         }
 
     });
 
+    // notifies the  chat that a message has been sent and sends it to all client sockets that are listening on that room.
     socket.on("transferMessage", (message)=>{
 
        io.sockets.to(socket.homeRoom).emit("notifyChat", socket.userName, message)
--- static/handlers.js
+++ static/handlers.js
@@ -1,61 +1,72 @@
 
+const clientSocket = io.connect("http://localhost:3000"); // creating a socket connection on port 3000.
+
+let homeRoom = "Community"; // default room the client is connected to.
 
 
-const clientSocket = io.connect("http://localhost:3000");
-
-let homeRoom = "Community";
-
-
+// adding a user to the program upon entering their name.
 function addUser()
 {
+    // gets the user UI tag and the userName value
     let userBox  = document.getElementById("userName");
     let userName = document.getElementById("user").value
     let chat = document.getElementById("chat");
 
-    console.log(userName)
+    
 
 
+    // checks if the userName is not undefined and that the string is a true string,
     if(userName!=undefined&&userName)
     {
-        userBox.hidden=true;
-        chat.style.visibility =  "visible";
-        clientSocket.emit("addUser", userName)
+        userBox.hidden=true; // hides the  userName UI 
+        chat.style.visibility =  "visible"; // hides username UI
+        clientSocket.emit("addUser", userName) // invokes the  servers add user functionality.
+
+        
 
     }
     else
     {
-        alert("Please enter a name. ")
+        alert("Please enter a name. ") 
     }
   
 
 }
 
+// updates the list of users in the program.
 clientSocket.on("updateList", function(data){
-    console.log("hello")
+   
    
     
 
-    let userList = document.getElementById("userList");
+    let userList = document.getElementById("userList"); // obtains the list html element.
 
     userList.innerHTML = "";
 
+    // for every user within the list enter as  a list element.
     for(var user in data )
     {
-        userList.innerHTML+=`<li>${user}</li>`
+        userList.innerHTML+=`<li style="color: blue">${user}</li>`
 
     }
 });
 
 
+// add chat functionality when a user creates a new chat room.
 function addChat()
 {
-    clientSocket.emit("newChat", document.getElementById("roomName").value)
-    document.getElementById("roomName").value = "";
+    
+    let room =  document.getElementById("roomName") // retreiving element
+    
+    clientSocket.emit("newChat", room.value) // invoking listener event on server side to create a new chat room.
+    room.value = "";
 
 }
 
+// allows the user to switch into a  new room upon clicking that room name.
 function switchRoom(newRoom)
 {
+    // if the newRoom is not the same room the user already resides, switch their room on the  server side and set the home room to the  new  room.
     if(newRoom!=homeRoom)
     {
         clientSocket.emit("switchRoom", newRoom)
@@ -65,42 +76,54 @@ function switchRoom(newRoom)
 }
 
 
+// sending message function.
 function transferMessage()
 {
-    let text = document.getElementById("userMessage").value;
-    clientSocket.emit("transferMessage", text);
+    let text = document.getElementById("userMessage").value; // obtains the message value.
+    clientSocket.emit("transferMessage", text); // sends the  message to the server  to broadcast to all users within the same room.
     text.value = "";
 
 }
 
+
+// updates the  chat window of the user.
 clientSocket.on("notifyChat", (userType, notifier)=>{
 
-    let chatMessage = document.getElementById("chatMessages")
+    let chatMessage = document.getElementById("chatMessages") // obtains  element
 
+    // checks if the message is an ADMIN message or not
     if(userType=="ADMIN")
     {
+       
         chatMessage.innerHTML+= "<p>" + notifier + "</p>";
 
     }
     else
     {
-        chatMessage.innerHTML+=`<p style="font-weight: bold"><span>${userType}</span></p>`
+        chatMessage.innerHTML+=`<p style="font-weight: bold"><span style="color:blue">${userType}:</span> ${notifier} </p>`
     }
 
+    // ensures the user  can  see all messages within the window.
     let messageContainer = document.getElementById("messageContainer");
     messageContainer.scrollTop = messageContainer.scrollHeight;
 
 
 });
 
+// switch room listener on client side that updates the rooms list on the front-end.
 clientSocket.on("switchRoom", (newRooms)=>{
 
-    let listOfRooms = document.getElementById("rooms");
+   
 
-    for(var room in newRooms)
+    let listOfRooms = document.getElementById("rooms"); // obtaining  element.
+    listOfRooms.innerHTML=``;
+
+    // loop through all rooms and add it to the list.
+    for(var num in newRooms)
     {
-        listOfRooms.innerHTML+=`<li id=${newRooms[room]} onclick = "switchRoom(${newRooms[room]})">`
-        listOfRooms.innerHTML+=`${newRooms[room]}</li>`
+        listOfRooms.innerHTML +=  `<li  id="${newRooms[num]}" onclick="switchRoom('${newRooms[num]}')" style="text-decoration: underline; color: green"> ${newRooms[num]} </li>`;
+    
+       
     }
 
 });
